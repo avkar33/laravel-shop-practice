b980ffc2952de6de70de5173f273dba9291d4bf2
avkar33 <kmi3342@protonmail.com>
2021-05-15 23:23:30 +0300

    update dasboard

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,23 +1,48 @@
 @extends('auth.layouts.master')
 
+@section('title', 'Заказы')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="col-md-12">
+        <h1>Заказы</h1>
+        <table class="table">
+            <tbody>
+            <tr>
+                <th>
+                    #
+                </th>
+                <th>
+                    Имя
+                </th>
+                <th>
+                    Телефон
+                </th>
+                <th>
+                    Когда отправлен
+                </th>
+                <th>
+                    Сумма
+                </th>
+                <th>
+                    Действия
+                </th>
+            </tr>
+            @foreach($orders as $order)
+                <tr>
+                    <td>{{ $order->id}}</td>
+                    <td>{{ $order->name }}</td>
+                    <td>{{ $order->phone }}</td>
+                    <td>{{ $order->created_at->format('H:i d/m/Y') }}</td>
+                    <td>{{ $order->getFullPrice() }} руб.</td>
+                    <td>
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-success" type="button"
+                               href="">Открыть</a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
-</div>
 @endsection
