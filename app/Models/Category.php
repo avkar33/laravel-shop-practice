973275d6dcdd36fcd6ed0f4b98e39ce923c307c4
avkar33 <kmi3342@protonmail.com>
2021-05-16 21:12:09 +0300

    add admin products crud

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'code', 'description'];
+    protected $fillable = ['name', 'code', 'description', 'image'];
 
     public function products()
     {
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +32,7 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
         Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
     });
     Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
 });
 
 
