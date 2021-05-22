cda5941c9ad45e1e214dd5bc46764f3a7db1ab43
avkar33 <kmi3342@protonmail.com>
2021-05-22 21:51:10 +0300

    add products query builder

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,9 +8,25 @@ use App\Models\Product;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::get();
+        $productsQuery = Product::query();
+
+        if ($request->filled('price_from')) {
+            $productsQuery->where('price', '>=', $request->price_from);
+        }
+
+        if ($request->filled('price_to')) {
+            $productsQuery->where('price', '<=', $request->price_to);
+        }
+
+        foreach (['hit', 'new', 'recommend'] as $field) {
+            if ($request->has($field)) {
+                $productsQuery->where($field, 1);
+            }
+        }
+
+        $products = $productsQuery->paginate(6)->withPath('?' . $request->getQueryString());
         return view('index', ['products' => $products]);
     }
     public function categories()
