d5641c8628c23defc49a953097009a8664e88db8
avkar33 <kmi3342@protonmail.com>
2021-07-03 22:09:56 +0300

    fix product subscribe

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -33,6 +33,25 @@
 
                         @else
                             <div class="alert-danger">Товар не доступен.</div>
+                            @if (Auth::user())
+                                @if (Auth::user()->products()->find($product->id))
+                                    <span class="btn-success">Вы подписаны</span>
+                                    <form method="POST" action="{{ route('unsubscribe', $product->id) }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger" role="button">Отписаться</button>
+                                    </form>
+                                @else
+                                    <form method="POST" action="{{ route('subscription', $product->id) }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-success" role="button">В желаемое</button>
+                                    </form>
+                                @endif
+                            @else
+                                <form method="POST" action="{{ route('subscription', $product->id) }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success" role="button">В желаемое</button>
+                                </form>
+                            @endif
                         @endif
 
                     </div>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -28,12 +28,19 @@
     @else
         <div class="alert-danger">Товар не доступен</div>
         <br>
-        @if (Auth::user()->products()->find($product->id))
-            <span class="btn-success">Вы подписаны</span>
-            <form method="POST" action="{{ route('unsubscribe', $product->id) }}">
-                @csrf
-                <button type="submit" class="btn btn-danger" role="button">Отписаться</button>
-            </form>
+        @if (Auth::user())
+            @if (Auth::user()->products()->find($product->id))
+                <span class="btn-success">Вы подписаны</span>
+                <form method="POST" action="{{ route('unsubscribe', $product->id) }}">
+                    @csrf
+                    <button type="submit" class="btn btn-danger" role="button">Отписаться</button>
+                </form>
+            @else
+                <form method="POST" action="{{ route('subscription', $product->id) }}">
+                    @csrf
+                    <button type="submit" class="btn btn-success" role="button">В желаемое</button>
+                </form>
+            @endif
         @else
             <form method="POST" action="{{ route('subscription', $product->id) }}">
                 @csrf
